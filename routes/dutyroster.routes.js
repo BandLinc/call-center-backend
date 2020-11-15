6443cf9bde9fe2b69f897049462e939f3834cd66
LincolnLidanya <lincolnlidanya@gmail.com>
2020-11-15 16:47:04 +0300

    Enable cors

--- routes/dutyroster.routes.js
+++ routes/dutyroster.routes.js
@@ -1,9 +1,10 @@
+var cors = require("cors");
 module.exports = (app) => {
   const dutyroster = require("../controllers/dutyroster.controller.js");
 
   // Retrieve all Customers
-  app.get("/dutyroster", dutyroster.findAll);
+  app.get("/dutyroster", dutyroster.findAll, cors());
 
   // Retrieve a single Customer with customerId
-  app.get("/dutyroster/:date", dutyroster.findOne);
+  app.get("/dutyroster/:date", dutyroster.findOne, cors());
 };
